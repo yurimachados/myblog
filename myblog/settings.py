b4aca2affd899334f1e95115a948c6b81da25eb4
Yuri Machado <ymachado1995@gmail.com>
2023-10-28 16:25:28 -0300

    feat: new dotenv.example configuration

--- myblog/settings.py
+++ myblog/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 
 from decouple import config
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=config('SECRET_KEY') 
+SECRET_KEY= os.environ.setdefault('SECRET_KEY', '123456abcdefgh')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +84,7 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.getenv('DATABASE_URL'),
     }
 }
 
